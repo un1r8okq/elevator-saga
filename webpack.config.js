602620dbf6a3e6d555da9ca1724d1b4df157d136
Will Verland <will@ver.land>
2021-08-29 12:45:15 +1200

    Convert to Typescript

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,18 @@
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: '/node_modules/',
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.ts'],
+  },
   output: {
     filename: 'index.js',
   },
